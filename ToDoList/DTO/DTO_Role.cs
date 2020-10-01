ae3bbd7dd07b6c88da985d684dfcb756ff1a5ea4
Nguyễn Hùng Vương <47838046+Vuongnguyen1705@users.noreply.github.com>
2020-10-01 23:36:47 +0700

    Add DTO class

--- ToDoList/DTO/DTO_Role.cs
+++ ToDoList/DTO/DTO_Role.cs
@@ -6,18 +6,15 @@ namespace DTO
 {
     class DTO_Role
     {
-        private int roleID;
-        private int roleName;
-
         public DTO_Role(int roleID, int roleName)
         {
-            this.RoleID = roleID;
-            this.RoleName = roleName;
+            RoleID = roleID;
+            RoleName = roleName;
         }
         public DTO_Role ()
         {
         }
-        public int RoleID { get => roleID; set => roleID = value; }
-        public int RoleName { get => roleName; set => roleName = value; }
+        public int RoleID { get; set; }
+        public int RoleName { get; set; }
     }
 }
